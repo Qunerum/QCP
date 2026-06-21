af135ca04c6dfdf1e8abacef4a922d30bf5cc1a7
Qunerum <qunerumofficial@gmail.com>
2026-06-21 18:16:52 +0200

    update 2026-06-21 18:16

--- include/main.h
+++ include/main.h
@@ -1,20 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef struct {
-    char* cmd;
-    void (*handler)(char*);
-} qcCmd;
-typedef struct {
-    int type;
-    char* name;
-    char* value;
-} qcVar;
-typedef struct {
-    char* name;
-    char* data;
-} qcFunc;
-
-
+typedef struct { char* cmd; void (*handler)(char*); } qcpCmd;
+typedef struct { int type; char* name; char* value; } qcpVar;
+typedef struct { char* name; char* data; } qcpFunc;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,15 +4,14 @@
 int main() {
     init_memory();
     FILE* file = fopen("test.qcp", "r");
-    if (!file) {
-        printf("Cannot open a file!\n");
-        return 1;
-    }
-
-    int symbol;
+    if (!file) { printf("Cannot open a file!\n"); return 1; }
+    int symbol, c;
     while ((symbol = fgetc(file)) != EOF) {
-        if (symbol != '\n') putchar(symbol);
+        if (symbol == '\n') continue;
+        else if (symbol == '{') { c++; continue; }
+        else if (symbol == '}' && c > 0) { c--; continue; }
     }
+    printf("%d", c);
     fclose(file);
     putchar(10);
     return 0;
